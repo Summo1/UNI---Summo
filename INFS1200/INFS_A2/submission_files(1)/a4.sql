bcd225043627ea9f6ff700de6d397cb4de9853f8
Summo1 <tomsummo@gmail.com>
2025-05-08 15:00:14 +1000

    A4

--- INFS1200/INFS_A2/submission_files(1)/a4.sql
+++ INFS1200/INFS_A2/submission_files(1)/a4.sql
@@ -1,4 +1,4 @@
 SELECT * FROM Role
-WHERE RoleID in(
-SELECT RoleID FROM Permission WHERE WebsiteURI LIKE ‘%.com’);
+WHERE RoleID IN(
+SELECT RoleID FROM Permission WHERE WebsiteURI LIKE '%.com');
 
