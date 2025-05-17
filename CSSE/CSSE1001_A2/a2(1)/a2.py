750f864c45075e75069f6096b512ee89ab75d23c
Summo1 <tomsummo@gmail.com>
2025-05-17 13:07:13 +1000

    Update a2.py

--- CSSE/CSSE1001_A2/a2(1)/a2.py
+++ CSSE/CSSE1001_A2/a2(1)/a2.py
@@ -8,7 +8,32 @@ from display import HearthView
 # -----------------------------------------------------------------------------
 
 # Define your classes and functions here
+class Card:
+    def __init__(self, name, description, cost, effect, symbol, **kwargs):
+       self.name = name
+       self.description = description
+       self.cost = cost
+       self.effect = effect
+       self.symbol = symbol
 
+    def __str__(self) -> str:
+        return f"{self.name} {self.description}"
+
+    def __repr__(self) -> str:
+        pass
+    def get_symbol(self) -> str:
+        return self.symbol
+    
+    def get_cost(self) -> int:
+        return self.cost
+
+    def get_effect(self) -> dict[str, int]:
+        return self.effect
+
+    def is_permanent(self) -> bool:
+        return 
+        
+        
 
 def main() -> None:
     pass
