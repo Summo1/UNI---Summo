2d65233f0c9a86b633a38cf626377c085259a236
Summo1 <tomsummo@gmail.com>
2025-05-17 15:11:01 +1000

    Testdeck

--- CSSE/CSSE1001_A2/a2(1)/a2.py
+++ CSSE/CSSE1001_A2/a2(1)/a2.py
@@ -81,13 +81,56 @@ class Fireball(Card):
             
     def increment_turn(self):
         self.turns_in_hand += 1
+        self.description = f"FIREBALL! Deals 3 + [turns in hand] damage. Currently dealing {3 + self.turns_in_hand} damage"
+        self.effect = {"damage": 3 + self.turns_in_hand}
+        self.symbol = str(self.turns_in_hand)
+
+
+class CardDeck():
+    def __init__(self, cards: list[Card]):
+        self.deck = cards
+        self.size = len(cards)
+        self.cards_left = len(cards)
+        
+        pass
+    def __str__(self) -> str:
+        for item in self.deck:
+            return item +','
+    def __repr__(self):
+        return 'CardDeck()'
+    def is_empty(self) -> bool:
+        if self.size == 0:
+            return True
+        else:
+            return False
+    def remaining_count(self) -> int:
+        return self.cards_left
     
+    def draw_cards(self, num: int) -> list[Card]:
+        drawn_cards = []
+        if num > self.size:
+            num = self.size
+        
+        drawn_cards = self.deck[:num]
+        self.deck = self.deck[num-1:]
+        
+        self.size -= num
+        self.cards_left -= num
+        
+        
+        return drawn_cards
+    
+    def add_card(self, card: Card):
+        self.deck.append(card)
+
     
     
         
     
 def main() -> None:
-    
+    cards = [Card(), Card(), Shield(), Heal(), Fireball(6)]
+    deck = CardDeck(cards)
+    print(deck.draw_cards(2))
     pass
 
 if __name__ == "__main__":
